cefd568623e262782fc8017a257d3462c6959195
giovannafer <143100572+giovannafer@users.noreply.github.com>
2023-10-05 10:50:31 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("black");
+  fill("pink");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+  let palavra = "Giovanna";
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+
+  
+
+}
